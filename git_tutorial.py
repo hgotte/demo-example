6dc29bd72ae9069993f8d2a296f23453a1a1c47f
hgotte <HG.Otte@outlook.de>
2025-08-01 15:51:21 +0200

    adding add_numbers function

---
+++ git_tutorial.py
@@ -0,0 +1,13 @@
+def add_number(a, b, c):
+    """
+    Adds three numbers together.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    c (int or float): The third number.
+    
+    Returns:
+    int or float: The sum of a, b, and c.
+    """
+    return a + b + c
