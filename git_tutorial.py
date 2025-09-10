844e8c4214fa7be00dd6ffce2ff471f4cb6eda4e
hgotte <HG.Otte@outlook.de>
2025-09-10 17:47:16 +0200

    again

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,16 +1,17 @@
-""" Module docstring """
+"""Module docstring"""
+
 
 def add_number(a, b, c):
+    """" adds three numbers together """
+    #   Adds three numbers together.
+    #   Parameters:
+    #   a (int or float): The first number.
+    #   b (int or float): The second number.
+    #   c (int or float): The third number.
+    #   Returns:
+    #   int or float: The sum of a, b, and c.
+    #
+    #   replace addition with sum
+    # add security check
 
-#   Adds three numbers together.
-#   Parameters:
-#   a (int or float): The first number.
-#   b (int or float): The second number.
-#   c (int or float): The third number.
-#   Returns:
-#   int or float: The sum of a, b, and c.
-#
-#   replace addition with sum
-# add security check
-
-     return sum([a, b, c])
+    return sum([a, b, c])
